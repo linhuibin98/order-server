f5deca8843fb1cd58eaeb2ab288a5c21a4f3a3c1
lhb1032352370 <1032352370@qq.com>
2020-01-02 12:30:10 +0800

    404

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const catchError = require('./middleWares/catchError');
 const connectDB = require('./middleWares/connectDB');
 const cors = require('koa2-cors');
 const fs = require('fs');
-const zlib = require('zlib');
 
 const app = new Koa();
 app.use(catchError());
@@ -19,14 +18,10 @@ app.use(registerRouter());
 
 // 404
 app.use(async (ctx, next) => {
-  if (ctx.url === '/') {
-    ctx.redirect('http://118.31.2.223/blog');
-  } else {
-    const rs = fs.createReadStream(path.resolve(__dirname, './public/404.html'), 'utf8');
-    ctx.status = 404;
-    ctx.type = 'html';
-    ctx.body = rs.pipe(zlib.createGunzip());
-  }
+  const rs = fs.createReadStream(path.resolve(__dirname, './public/404.html'), 'utf8');
+  ctx.status = 404;
+  ctx.type = 'html';
+  ctx.body = rs;
 })
 
 app.listen(8080, () => {
